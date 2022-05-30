445c937890b71778b341bd57e91301b96af1ae02
Joanna Biegala <joannabiegala0511@gmail.com>
2022-05-30 11:09:50 +0200

    In the second method, call the first method and negate the result. I divided the tests into two per method and changed their nomenclature

--- src/main/java/com/futurecollars/Main.java
+++ src/main/java/com/futurecollars/Main.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in) ;
-        System.out.println("Podaj liczbę:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę aby sprawdzić jej parzytość i nieparzystość:");
         int number = scanner.nextInt();
-        System.out.println(isEvenNumber(number));
-        System.out.println(isOddNumber(number));
-     }
-     public static boolean isEvenNumber(int number) {
-        int modulo =  number%2;
-        return modulo == 0;
+        System.out.println(isEvenNumber(number) ? "Liczba jest parzysta" : "Liczba nie jest parzysta");
+        System.out.println(isOddNumber(number) ? "Liczba jest nieparzysta" : "Liczba nie jest nieparzysta");
     }
+
+    public static boolean isEvenNumber(int number) {
+        return number % 2 == 0;
+    }
+
     public static boolean isOddNumber(int number) {
-        int modulo =  number%2;
-        return modulo != 0;
+        return !isEvenNumber(number);
     }
 }
--- src/test/java/com/futurecollars/MainTest.java
+++ src/test/java/com/futurecollars/MainTest.java
@@ -5,34 +5,56 @@ import org.junit.jupiter.api.Assertions;
 
 class MainTest {
     @Test
-    void isEvenNumber() {
+    void shouldBeEvenWhenCheckingIfEven() {
         //given
         int evenNumber = 20;
-        int oddNumber = 15;
 
         //when
-        boolean trueExpected = Main.isEvenNumber(evenNumber);
-        boolean falseExpected = Main.isEvenNumber(oddNumber);
+        String evenExpected = Main.isEvenNumber(evenNumber) ? "Liczba jest parzysta" : "Liczba nie jest parzysta";
 
         //then
-        Assertions.assertTrue(trueExpected);
-        Assertions.assertFalse(falseExpected);
+        Assertions.assertEquals("Liczba jest parzysta", evenExpected);
 
     }
+
     @Test
-    void isOddNumber() {
+    void shouldNotBeEvenWhenCheckingIfEven() {
         //given
-        int evenNumber = 20;
         int oddNumber = 15;
 
         //when
-        boolean trueExpected = Main.isOddNumber(oddNumber);
-        boolean falseExpected = Main.isOddNumber(evenNumber);
+        String notEvenExpected = Main.isEvenNumber(oddNumber) ? "Liczba jest parzysta" : "Liczba nie jest parzysta";
 
         //then
-        Assertions.assertTrue(trueExpected);
-        Assertions.assertFalse(falseExpected);
+        Assertions.assertEquals("Liczba nie jest parzysta", notEvenExpected);
 
+
+    }
+
+    @Test
+    void shouldBeOddWhenCheckingIfOdd() {
+        //given
+        int oddNumber = 15;
+
+        //when
+        String oddExpected = Main.isOddNumber(oddNumber) ? "Liczba jest nieparzysta" : "Liczba nie jest nieparzysta";
+
+        //then
+        Assertions.assertEquals("Liczba jest nieparzysta", oddExpected);
+
+
+    }
+
+    @Test
+    void shouldNotBeOddWhenCheckingIfOdd() {
+        //given
+        int evenNumber = 20;
+
+        //when
+        String notOddExpected = Main.isOddNumber(evenNumber) ? "Liczba jest nieparzysta" : "Liczba nie jest nieparzysta";
+
+        //then
+        Assertions.assertEquals("Liczba nie jest nieparzysta", notOddExpected);
 
     }
 }
